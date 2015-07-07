8677747a23f068322895638ec27618ef1863cefc
Mandeep <mandeep@stackhut.com>
2015-07-07 10:49:55 +0100

    Bump version: 0.1.11 → 0.1.12

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ test_requirements = [
 
 setup(
     name='stackhut',
-    version='0.1.11',
+    version='0.1.12',
     description="Run your software in the cloud",
     long_description=(read('README.rst') + '\n\n' +
                       read('HISTORY.rst').replace('.. :changelog:', '') + '\n\n' +
