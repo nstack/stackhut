54751ee87704ef112944af07be498f411263029f
Mandeep <mandeep@stackhut.com>
2015-07-12 23:01:56 +0100

    Bump version: 0.1.21 → 0.1.22

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ test_requirements = [
 
 setup(
     name='stackhut',
-    version='0.1.21',
+    version='0.1.22',
     description="Run your software in the cloud",
     long_description=(read('README.rst') + '\n\n' +
                       read('HISTORY.rst').replace('.. :changelog:', '') + '\n\n' +
--- stackhut/__init__.py
+++ stackhut/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 __author__ = 'StackHut'
 __email__ = 'stackhut@stackhut.com'
-__version__ = '0.1.21'
+__version__ = '0.1.22'
 
 from .__main__ import main
 from .utils import log
