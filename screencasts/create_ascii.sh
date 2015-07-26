29b4adda229d413c3d7b46af566f82cae8bd77dc
Mandeep <mandeep@stackhut.com>
2015-07-26 17:25:24 +0100

    update screencasts

--- screencasts/create_ascii.sh
+++ screencasts/create_ascii.sh
@@ -1,8 +1,8 @@
 # StackHut allows you to rapidly deploy your code as an API in the cloud
 # this screencast shows creating and deploying a simple service on StackHut
 
-# first make sure we have stackhut installed
-pip3 install --user stackhut
+# first make sure we have StackHut (and Docker) installed
+sudo pip3 install stackhut > /dev/null
 stackhut -V
 
 # login/register with Docker (to store your StackHut services)
@@ -12,55 +12,58 @@ docker login
 stackhut login
 stackhut info
 
-# let's create a Python project using Alpine Linux
+# let's create a Python 3 project using Alpine Linux
 mkdir demo-python
 cd demo-python
 stackhut init alpine python
 
-# and we're good to go, the project is set up, with a Git repo too - aren't we nice...
+# the project is set up, with a Git repo too - aren't we nice...
 ls
 
 # the ``Hutfile`` is a YAML config file regarding our stack and dependencies
-alias ccat pygmetize
+alias ccat pygmentize
 ccat -l yaml ./Hutfile
 
-# there's also a README.md to describe your service
-cat README.md
-
-# api.idl describes our service interface, these entrypoints will be accessible over HTTP
-# it uses a Java-like syntax to describe the service interface using JSON types (http://barrister.bitmechanic.com/docs.html)
+# api.idl describes our service interface with a Java-like syntax
+# these entrypoints will be accessible over HTTP
 # let's take a look
 ccat -l java api.idl
 
-# we are exposing a single function, add, that takes two ints, and returns an int
-# now let's write the signature for a new function, 'multiply' 
-nano api.idl
+# we're exposing a single function, "add"
+# let's write the signature for a new function, "multiply" 
 # yes - nano ftw!
+nano api.idl
+
+// multiply 2 integers and return the result
+multiply(x int, y int) int
 
 # now we may write our code
-# this lives in app.py (or app.js for JS, and so on)
+# this lives in app.py (or app.js for JS, etc.)
 ccat app.py
 
-# the service is a plain old Python class with a function for each entrypoint
-# 'add' has already been implemented, let's write 'multiply'
+# the service is a Python class with a function for each entrypoint
+# "add" has already been implemented, let's write "multiply"
 nano app.py
 
-# we're done coding, and can now build, run, and test our service before we deploy
-# let's build our service, i.e. packaging up everything into a container image
+def multiply(self, x, y):
+    return x * y
+
+# we're done coding, and can build and run our service locally
+# first let's build our service, i.e. packaging into an image
 stackhut build
 
-# great...but let's test our service before deploying
+# great, and now let's test our service
 # the file 'test_request.json' models a HTTP request to the API
-# this is a JSON-RPC object that describes the service, method, and parameters
+# this is a JSON-RPC object that describes the call
 ccat ./test_request.json
 
-# let's run our service with this file to test our 'add' function
+# we'll run our service with this file to test "add"
 stackhut run test_request.json
 
-# that worked, and we can view the JSON-RPC reposonse in ./run_result/response.json
+# that worked, we can view the response in ./run_result/response.json
 ccat ./run_result/response.json
 
-# let's modify 'test_request.json' to test our multiply function
+# let's modify "test_request.json" to test our multiply function
 nano ./test_request.json
 stackhut run test_request.json
 ccat ./run_result/response.json
@@ -72,7 +75,8 @@ git commit -am "Working service"
 # this couldn't be simpler
 stackhut deploy
 
-# great, your API is live on https://api.stackhut.com/run and can be browsed from https://www.stackhut.com/#/services
-# it can receive requests from anywhere via HTTP, as shown in http://docs.stackhut.com/getting_started/tutorial_use.html
-# Thanks for your time - we can't wait to see what you build... 
+# great, the API is live, see https://www.stackhut.com/#/u/mands/demo-python
+# it can be used via a HTTP POST to https://api.stackhut.com/run
+# as shown in http://docs.stackhut.com/getting_started/tutorial_use.html
+# thanks for your time - we can't wait to see what you build... 
 
--- screencasts/use_ascii.sh
+++ screencasts/use_ascii.sh
@@ -1,37 +1,47 @@
-# this screencast shows using a StackHut service from your application
+# this screencast shows how to use a StackHut service from your code
 # whether in Python, JS, Ruby ... or even the shell! 
 # you can find many services at https://stackhut.com/#/services
-# we will be using the service 'mands/demo-python'
-# created in http://docs.stackhut.com/getting_started/tutorial_create.html
+# we will be using the service "mands/demo-python"
+# (as created in http://docs.stackhut.com/getting_started/tutorial_create.html)
 
 # let's first view the documentation
-open http://stackhut.com/#/u/mands/demo-python
+open "http://stackhut.com/#/u/mands/demo-python"
 
-# it has 2 methods, 'add' and 'multiply'. We can call these via a JSON-RPC HTTP request
+# so it has 2 methods, "add" and "multiply", we can call these via a JSON-RPC HTTP request
 # thankfully JSON-RPC is very simple!
-# let's write some JSON to call 'add' from 'mands/demo-python' with 2 parameters
+# let's write some JSON to call "add" from 'mands/demo-python' with 2 parameters
 mkdir demo
 cd demo
 nano test_request.json
 
-# we can call this simply through a HTTP POST request to https://api.stackhut.com/run
-# let's use the 'http' tool (https://github.com/jkbrzt/httpie)
+
+{
+    "service": "mands/demo-python",
+    "req": {
+        "method": "add",
+        "params": [2, 2]
+    }
+}
+
+
+# we can call this through a HTTP POST request to https://api.stackhut.com/run
+# let's use the "http" tool (https://github.com/jkbrzt/httpie)
 http POST https://api.stackhut.com/run @./test_request.json 
 
-# the JSON-RPC response contains the return value in the 'result' field
-# let's pipe into 'jq' (http://stedolan.github.io/jq/)
-http POST https://api.stackhut.com/run @./test_request.json | jq '.result'
+# the JSON-RPC response contains the return value in the "result" field
+# let's pipe it into "jq" (http://stedolan.github.io/jq/)
+http POST https://api.stackhut.com/run @./test_request.json | jq '.response.result'
 
-# so it turns out that 2 plus 2 does equals 4, great!
+# so it turns out that 2 + 2 = 4, great! :)
 # and if we send invalid data...
 nano test_request.json
 http POST https://api.stackhut.com/run @./test_request.json 
 
-# the JSON-RPC response now has an 'error' field
+# the JSON-RPC response now has an "error" field
 # the system caught the mistake and you can deal with it programmatically
-http POST https://api.stackhut.com/run @./test_request.json | jq '.error.code'
+http POST https://api.stackhut.com/run @./test_request.json | jq '.response.error.code'
 
 # we hope this shows how you can call any StackHut service from your code
-# it's as easy as making a JSON POST request
+# it's as easy as making a JSON-based POST request
 # thanks for your time - we can't wait to see how you make use of it... 
 
