b9d3ae9850693a6ca1d31ea14a3c4f7b098ea1d3
Mandeep <mandeep@stackhut.com>
2015-07-28 15:37:05 +0100

    Bump version: 0.3.14 → 0.3.15

--- stackhut/__init__.py
+++ stackhut/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 __author__ = 'StackHut'
 __email__ = 'stackhut@stackhut.com'
-__version__ = '0.3.14'
+__version__ = '0.3.15'
 
 from .__main__ import main
 from stackhut.common.utils import log
